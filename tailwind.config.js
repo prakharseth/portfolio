d6fa881eb423f84dd04386aa857e471901b60018
utkarshseth2019@gmail.com <utkarshseth2019@gmail.com>
2022-04-05 19:43:16 +0530

    starting-1st

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        handlee: ["Handlee", "sans-serif"],
+        open: ["Open Sans", "cursive"],
+        play: ["Play", "cursive"],
+      },
+    },
+  },
+  plugins: [],
+};
